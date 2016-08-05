202cc965c8278c8c1fa565e1319cc2639522b90a
jgiacomo <jgiacomo@gmail.com>
2016-08-04 21:23:47 -0700

    outline of function.

--- helperFunctions/NearestStdRuns.R
+++ helperFunctions/NearestStdRuns.R
@@ -1,1 +1,19 @@
-NearestStdRuns <- function(){}
+NearestStdRuns <- function(df, run, standardPos, n=6){
+    # Finds standard runs (from the list of standards) which are nearest in time
+    # to the unknown's run. The number of standard runs found is determined by
+    # n.
+    
+    # Inputs
+    #     df = data frame containing AMS run data
+    #     run = the run number of the unknown
+    #     standardPos = a list of the positions of standards
+    #     n = number of nearest standard runs to find
+    #
+    # Outputs
+    #     nearestStdRuns = a list of the nearest standard runs
+    
+    # Check that the run completion times are formatted as date times (POSIXlt)
+    try
+    
+    
+}
