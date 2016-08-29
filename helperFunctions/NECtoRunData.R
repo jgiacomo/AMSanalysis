a38046fd28d1d081f2689ede2804e6c968984018
Jason Giacomo <jgiacomo@gmail.com>
2016-08-28 20:48:45 -0700

    completed function

--- helperFunctions/NECtoRunData.R
+++ helperFunctions/NECtoRunData.R
@@ -7,10 +7,33 @@ NECtoRunData <- function(runlog){
     
     # Value: a data frame of AMS results in a universal format.
     
-    source("NEC_runlog_to_dataframe.R")  # convert runlog to data frame.
+    library(readr)
+    library(stringr)
     
-    df <- NECrunlogToDF(runlog, lab="accium")
+    # create column widths, types, and read fixed-width-file to data frame.
+    colWidths <- fwf_empty(runlog, skip=5)
+    colTypes <- "ccciicciddiiiddd"
+    df <- read_fwf(runlog, colWidths, skip=6, col_types=colTypes)
     
-    df$Run.Completion.Time <- strptime(df$Run.Completion.Time, format='%c')
+    colNames <- c("E","Item","runTime","pos","meas","smType",
+                  "sampleName","numCycles","he12C","he13C","cntTotH",
+                  "cntTotS","cntTotGT","he13.12","he14.12","he14.13")
+    names(df) <- colNames
+
+    # coerce to the proper time format (POSIXct required by dplyr)
+    df$runTime <- as.POSIXct(strptime(df$runTime, format='%c'))
+    
+    # create missing columns, batch inferred from runlog file
+    batch <- str_extract(readLines(runlog, n=3)[3], '[bB]atch[^\\s]*')
+    df$analysis <- batch  # This is the batch number or magazine number.
+    df$trans12C <- NA  # Doesn't exist yet for the NEC instrument.
+    df$active <- TRUE  # Initially we make all runs active.
+    df$run <- paste(df$analysis, df$pos, df$meas, sep="-")
+    df$count14C <- df$cntTotGT
+    
+    # Now to adjust columns to those for the general rundata data frame
+    rundata <- df %>% select(analysis, sampleName, smType, pos, run, runTime,
+                             numCycles, he12C, he13C, count14C, he13.12,
+                             he14.12, he14.13, trans12C, active)
     
 }
