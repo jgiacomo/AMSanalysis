932436643493d2bfdb9422dd91906517dd00d823
jgiacomo <jgiacomo@gmail.com>
2019-05-14 05:58:39 -0700

    Merge changes from modularize-shiny branch, but only for this file.

--- RadiocarbonDates.R
+++ RadiocarbonDates.R
@@ -3,13 +3,14 @@
 
 # -------------------------include libraries-----------------------------------
 
-library(doBy)  # used for summary statistics on data.frames
 library(SDMTools)  # weighted means and variance calculations
 library(dplyr)
+library(readr)
 
 # -------------------------source files----------------------------------------
 
 source("helperFunctions/numInputToIntegers.R")
+source("helperFunctions/NearestStdRuns.R")
 
 # -------------------------define constants------------------------------------
 
@@ -18,56 +19,9 @@ kPileUp <- 3.5E-6  # pile up time constant for detector
 kElemQ <- 1.6022E-19  # elementary charge
 kVPDB <- 0.0112372  # Vienna PeeDee Belemnite standard 13C/12C ratio
 kOX2ModernFactor <- 0.7459  # Modern = this * normalized 14C/12C of standards
+kOX1ModernFactor <- 0.95  # OX1 modern, convert d13C (-19)
 
 # -------------------------define Functions------------------------------------
-
-NearestStdRuns <- function(run, data.set, sampletype) {
-    # This function takes as input the run from which you wish to find the 
-    # nearest standard runs and the data.frame you wish to work with. The 
-    # function returns a vector containing the run numbers for the nearest 
-    # standard measurements. It will find the nearest 6 standard runs, both 
-    # forwards and back, or less if there are less than 6 standard runs. It 
-    # will also exclude the run which is input in case the run you are 
-    # searching from is a standard measurement itself. The output can be used 
-    # to determine the correct normalization for any unknown sample or even a 
-    # standard as the run being normalized will not be included in the 
-    # calculation of the normalization thereby preventing self-normalized 
-    # results.
-    
-    # get the run numbers for the standards assuming the data.frame has a RUN
-    # column.
-    std.runs <- data.set[data.set$smType == sampletype,]
-    
-    # use only active standard runs
-    std.runs <- std.runs[std.runs$active,]
-    
-    # remove the run specified from the standard runs if it is a standard
-    std.runs <- std.runs[std.runs$run != run, ]
-    
-    # find the date and time of the run specified
-    run.time <- data.set[data.set$run == run, ]$dateTime
-    
-    # find the difference between the time of the run and all standard runs
-    std.runs$TimeDiff <- abs(as.numeric(difftime(std.runs$dateTime, run.time)))
-    
-    std.runlist <- c()  # initialize a list to hold the runs
-    
-    # set loop count to number of rows in std.runs or 6 whichever is smaller
-    if(nrow(std.runs) < 6) {
-        count <- nrow(std.runs)
-    }
-    else {
-        count = 6
-    }
-    
-    # find nearest standard run then remove that run and repeat to count
-    for(i in 1:count) {
-        min.run <- std.runs[which.min(std.runs$TimeDiff), ]$run
-        std.runlist <- c(std.runlist, as.character(min.run))
-        std.runs <- std.runs[std.runs$run != min.run, ]
-    }
-    return(std.runlist)
-}
 
 Convertd13C <- function(c13.sample, c13.vpdb, bool = TRUE) {
     # This function takes the sample 13C/12C ratio or the sample d13C and the 
@@ -111,6 +65,20 @@ wt.summary <- function(x, error){
 # Remove all inactive runs from the data
 run.data <- rundata %>% filter(active==TRUE)
 
+# Which standard to use.
+cat("Choose standard\n")
+cat("Enter '1' for OXI (OX1)\n")
+cat("Enter '2' for OXII (OX2)\n")
+std.answer <- readline("Choice:")
+if(std.answer=="1"){
+    std.type <- "OX1"
+} else if(std.answer=="2"){
+    std.type <- "OX2"
+} else {
+    stop("Your answer was not recognised.")
+}
+
+
 # Get info about the blanks.
 cat("Machine Blank\n")
 cat("Enter '1' to choose positions for the machine blank.\n")
@@ -125,9 +93,11 @@ if(mb.answer=="1"){
                          run.data[run.data$pos %in% mb.pos,]$he14.13.error)[[1]]
 } else if(mb.answer=="2"){
     cat("Enter the 14C/12C or 14C/13C ratio for the machine blank\n")
-    mb.ratio <- readline("Ratio:")
+    mb.ratio <- as.numeric(readline("Ratio:"))
+    mb.pos <- 1000  # Unrealistic position since there is presumably no MB.
 } else if(mb.answer=="3"){
     mb.ratio <- 0
+    mb.pos <- 1000  # Unrealistic position since there is presumably no MB.
 } else {
     stop("Your answer was not recognised.")
 }
@@ -146,6 +116,20 @@ if(cb.answer=="1"){
 } else if(cb.answer=="3"){
     cb.ratio <- 0
     cb.error <- 0
+    cb.pos <- 1000  # Unrealistic position since there is presumably no CB.
+} else {
+    stop("Your answer was not recognised.")
+}
+
+cat("\nDead Time Correction\n")
+cat("Enter '1' to use a dead time correction.\n")
+cat("Enter '2' for no dead time correction.\n")
+dtc.answer <- readline("Choice:")
+if(dtc.answer=="1"){
+    dt.value <- readline("dead time in micro seconds:")
+    dt.value <- as.numeric(dt.value) * 1e-6
+} else if(dtc.answer=="2"){
+    dt.value <- 0  # No dead time correction
 } else {
     stop("Your answer was not recognised.")
 }
@@ -153,33 +137,47 @@ if(cb.answer=="1"){
 # Setup new columns
 run.data$std.runs <- NA
 run.data$d13C <- NA
-run.data$he14.13.mb <- NA
+run.data$DTC <- NA  # dead time correction = 1/(1-dt*countrate)
+run.data$he14.13.dtc <- NA  # dead time corrected 14C/13C
+run.data$he14.13.mb <- NA   # maching background subtracted 14C/13C
 run.data$he14.13.mb.error <- NA
-run.data$he14.13.d13C <- NA
+run.data$he14.13.d13C <- NA # delta 13C corrected 14C/13C
 run.data$he14.13.d13C.error <- NA
 run.data$pMC <- NA
 run.data$pMC.error <- NA
 
+# Apply the dead time correction (cycles are 0.1 seconds)
+run.data$DTC <- 1 / (1 - dt.value*run.data$count14C/run.data$numCycles*10)
+run.data$he14.13.dtc <- run.data$he14.13 * run.data$DTC
+
 # Subtract the machine blank, but not from the machine blank.
 run.data[!(run.data$pos %in% mb.pos),]$he14.13.mb <-
-    run.data[!(run.data$pos %in% mb.pos),]$he14.13 - mb.ratio
+    run.data[!(run.data$pos %in% mb.pos),]$he14.13.dtc - mb.ratio
 run.data[run.data$pos %in% mb.pos,]$he14.13.mb <-
     run.data[run.data$pos %in% mb.pos,]$he14.13
 run.data$he14.13.error.mb <- run.data$he14.13.error
 
 # For each run, find the nearest standard runs and d13C values
+if(std.type=="OX1"){
+    std.pos <- unique(run.data$pos[run.data$smType %in% c("OX1","OXI")])
+} else{
+    std.pos <- unique(run.data$pos[run.data$smType %in% c("OX2","OXII")])
+}
+
 for(i in 1:nrow(run.data)){
     # Find the nearest standards
     run <- run.data[i,]$run
-    std.runlist <- NearestStdRuns(run, run.data, "OX2")
+    std.runlist <- NearestStdRuns(run.data, run, std.pos, n=6)
     run.data[i,]$std.runs <- list(std.runlist)
-    std.data <- run.data[run.data$run %in% std.runlist,]
+    std.data <- run.data[run.data$run %in% std.runlist$run,]
 
     # Calculate normalized d13C results
     std.regression <- lm(std.data$he13.12 ~ std.data$he13C)
     reg.13.12 <- run.data[i,]$he13C * std.regression$coefficients[2] +
         std.regression$coefficients[1]
-    norm.13.12 <- Convertd13C(-17.8,kVPDB,FALSE) / reg.13.12
+    if(std.type=="OX1"){std.d13C <- -19}
+    if(std.type=="OX2"){std.d13C <- -17.8}
+    norm.13.12 <- Convertd13C(std.d13C,kVPDB,FALSE) / reg.13.12
     smpl.13.12 <- run.data[i,]$he13.12 * norm.13.12
     run.data[i,]$d13C <- Convertd13C(smpl.13.12,kVPDB,TRUE)
     
@@ -192,6 +190,14 @@ run.data$he14.13.d13C <- run.data$he14.13.mb *
                          (-25/1000 + 1)/(run.data$d13C/1000 + 1)
 run.data$he14.13.d13C.error <- run.data$he14.13.error.mb *
                          (-25/1000 + 1)/(run.data$d13C/1000 + 1)
+if(std.type=="OX1"){
+    run.data[run.data$smType=="OX1",]$he14.13.d13C <-
+        run.data[run.data$smType=="OX1",]$he14.13.mb *
+        (-19/1000 + 1)/(run.data[run.data$smType=="OX1",]$d13C/1000 + 1)
+    run.data[run.data$smType=="OX1",]$he14.13.d13C.error <-
+        run.data[run.data$smType=="OX1",]$he14.13.error.mb *
+        (-19/1000 + 1)/(run.data[run.data$smType=="OX1",]$d13C/1000 + 1)
+}
 
 # Get the chemical blank value
 if(cb.answer=="1"){
@@ -202,32 +208,35 @@ if(cb.answer=="1"){
 }
 
 # Subtract chemical blank and calculate the standard normalized pMC
+if(std.type=="OX1"){ModernFactor <- kOX1ModernFactor}
+if(std.type=="OX2"){ModernFactor <- kOX2ModernFactor}
+
 for(i in 1:nrow(run.data)){
-    std.runs <- run.data[i,]$std.runs[[1]]
+    std.runs <- run.data$std.runs[[i]]$run
     std.data <- run.data[run.data$run %in% std.runs,]
     std.results <- wt.summary(std.data$he14.13.d13C,
                               std.data$he14.13.d13C.error)
     
     if(!(run.data[i,]$pos %in% c(mb.pos, cb.pos))){
         run.data[i,]$pMC <- (run.data[i,]$he14.13.d13C - cb.ratio) /
-            (std.results[[1]] - cb.ratio) / kOX2ModernFactor * 100
+            (std.results[[1]] - cb.ratio) / ModernFactor * 100
         
         run.data[i,]$pMC.error <- sqrt(
             (run.data[i,]$he14.13.d13C.error/(std.results[[1]]-cb.ratio) /
-                 kOX2ModernFactor)^2 +
+                 ModernFactor)^2 +
                 (std.results[[2]]*(run.data[i,]$he14.13.d13C-cb.ratio) /
-                     kOX2ModernFactor / (std.results[[1]]-cb.ratio)^2)^2 +
+                     ModernFactor / (std.results[[1]]-cb.ratio)^2)^2 +
                 (cb.error/cb.ratio)^2
         )
     } else{
         run.data[i,]$pMC <- (run.data[i,]$he14.13.d13C) /
-            (std.results[[1]]) / kOX2ModernFactor * 100
+            (std.results[[1]]) / ModernFactor * 100
         
         run.data[i,]$pMC.error <- sqrt(
             (run.data[i,]$he14.13.d13C.error/(std.results[[1]]) /
-                 kOX2ModernFactor)^2 +
+                 ModernFactor)^2 +
                 (std.results[[2]]*(run.data[i,]$he14.13.d13C) /
-                     kOX2ModernFactor / (std.results[[1]])^2)^2
+                     ModernFactor / (std.results[[1]])^2)^2
         )
     }
 }
@@ -238,7 +247,8 @@ finalResults <- run.data %>% group_by(pos,analysis,label,smType) %>%
               mn.d13C=mean(d13C),d13C.error=sd(d13C)/sqrt(n()-1),
               mn.pMC=mean(pMC),pMC.error=sd(pMC)/sqrt(n()-1)) %>%
     mutate(Age=round(-8033*log(mn.pMC/100),0),
-           Age.error=round(8033*pMC.error/mn.pMC,0))
+           Age.error=round(8033*pMC.error/mn.pMC,0)) %>%
+    rename(d13C=mn.d13C,pMC=mn.pMC)
 
 
 filename <- paste0(finalResults[1,]$analysis,"_finalResults.csv")
